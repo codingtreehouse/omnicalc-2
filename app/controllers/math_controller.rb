9490e8a86ac7233a33642a828567245daa227329
Andrew Johnson <andrewj@chicagobooth.edu>
2023-04-13 15:28:34 +0000

    completed

--- app/controllers/math_controller.rb
+++ app/controllers/math_controller.rb
@@ -7,7 +7,47 @@ class MathController < ApplicationController
     @third = @first + @second
     render({:template => "math_templates/addition_results.html.erb"})
   end
-  def gotoform
+
+  def gotoadd
     render({:template => "math_templates/add.html.erb"})
+  end
+
+  def gotosubtract
+    render({:template => "math_templates/subtract.html.erb"})
+  end
+
+  def subtract_results
+    # {"first_num"=>"42", "second_num"=>"34"}
+
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @third = @second - @first
+    render({:template => "math_templates/subtraction_results.html.erb"})
+  end
+
+  def gotomultiply
+    render({:template => "math_templates/multiply.html.erb"})
+  end
+
+  def multiply_results
+    # {"first_num"=>"42", "second_num"=>"34"}
+
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @third = @second * @first
+    render({:template => "math_templates/multiply_results.html.erb"})
+  end
+
+  def gotodivide
+    render({:template => "math_templates/divide.html.erb"})
+  end
+
+  def divide_results
+    # {"first_num"=>"42", "second_num"=>"34"}
+
+    @first = params.fetch("first_num").to_f
+    @second = params.fetch("second_num").to_f
+    @third = @first / @second
+    render({:template => "math_templates/divide_results.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get("/wizard_add", {:controller => "math", :action => "add_results"})
-  get("/add", {:controller => "math", :action => "gotoform"})
+  get("/add", {:controller => "math", :action => "gotoadd"})
+
+  get("/wizard_subtract", {:controller => "math", :action => "subtract_results"})
+  get("/subtract", {:controller => "math", :action => "gotosubtract"})
+
+  get("/wizard_multiply", {:controller => "math", :action => "multiply_results"})
+  get("/multiply", {:controller => "math", :action => "gotomultiply"})
+
+  get("/wizard_divide", {:controller => "math", :action => "divide_results"})
+  get("/divide", {:controller => "math", :action => "gotodivide"})
 
 end
